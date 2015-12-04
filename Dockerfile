d7271170be59356e46b37e82a599ff23f9bd06dd
Ben Smith <benjsmi@us.ibm.com>
2015-12-04 09:19:39 -0500

    See what this breaks...

--- Dockerfile
+++ Dockerfile
@@ -3,16 +3,13 @@ FROM    node:0.10
 MAINTAINER Ben Smith (benjsmi@us.ibm.com)
 
 ADD https://download.elastic.co/logstash-forwarder/binaries/logstash-forwarder_linux_amd64 /opt/forwarder
-ADD https://admin:PLACEHOLDER_ADMIN_PASSWORD@game-on.org:8443/logstashneeds.tar /opt/logstashneeds.tar
 
 ADD ./src/ /opt/docs/
 
 RUN cd /opt/docs/ ; npm install gitbook-cli
 
-#RUN cd /opt ; chmod +x ./forwarder ; tar xvzf logstashneeds.tar ; rm logstashneeds.tar ; \
-#	echo "Installing Node modules..." ; cd /opt/room ; npm install 
-
 COPY ./startup.sh /opt/startup.sh
+COPY ./forwarder.conf /opt/forwarder.conf
 
 EXPOSE 4000
 
--- startup.sh
+++ startup.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+if [ "$LOGGING_DOCKER_HOST" != "" ]; then
+  echo Starting the logstash forwarder...
+  wget https://admin:$ADMIN_PASSWORD@game-on.org:8443/logstashneeds.tar -O /opt/logstashneeds.tar
+  sed -i s/PLACEHOLDER_LOGHOST/$LOGGING_DOCKER_HOST/g /opt/forwarder.conf
+  tar xvzf logstashneeds.tar ; rm logstashneeds.tar
+  cd /opt ; chmod +x ./forwarder ; tar xvzf logstashneeds.tar ; rm logstashneeds.tar
+  ./forwarder --config ./forwarder.conf &
+  echo "Starting the GitBook Server"
+  cd /opt/docs
+  node_modules/.bin/gitbook serve
+else
+  echo "No logging configured; GitBook will log to STDOUT."
+  cd /opt/docs
+  node_modules/.bin/gitbook serve
+fi
 
-
-cd /opt/docs
-
-echo "Starting the GitBook Server"
-node_modules/.bin/gitbook serve
-
-#echo Running Logstash Forwarder...
-#sed -i s/PLACEHOLDER_LOGHOST/$LOGGING_DOCKER_HOST/g /opt/forwarder.conf
-#cd /opt ; ./forwarder --config ./forwarder.conf
